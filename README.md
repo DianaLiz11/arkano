3afff8cd6b8b49078dce2a2fbdc941a7bf3a237b
DianaLiz11 <dianamunoz86@live.com.mx>
2019-07-01 10:56:49 -0500

    Se corrige submit del button del form

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# arkano
+# ARKANO
+
+Arkano es un aplicativo web de ventas,el cual puede ser accedido de múltiples dispositivos.
+
+El aplicativo contiene dos secciones, la primera es el Registro y es donde el usuario puede dar de alta los datos de Dashboard.
+
+La segunda sección es Dashboard, y en esta se puden ver los datos registrados en la página de Registro.
+
+Para ver el proyecto puede dar clic [aquí](https://dianaliz11.github.io/arkano/src/).
+
+El proyecto se encuentra desarrollado en Javascript Vanilla, HTML 5, CSS 3 y bootstrap. y esta alojado en GitHub Pages.
+
+Opté por usar Javascript Vanilla por tratarse de una página que unicamente contiene dos vistas.
+
+Y en el caso de bootstrap, tomé la decision de usarlo porque facilita el acomodo de elementos por el sistema de GRID que utiliza.
--- src/main.js
+++ src/main.js
@@ -4,16 +4,20 @@ const titleShopping = document.getElementById('title-shopping');
 const titleIncremet = document.getElementById('title-incremet');
 const titleUsers = document.getElementById('title-users');
 const titleVisit = document.getElementById('title-visit');
-
-// let data={};
+const boardNameForm = document.getElementById('board-name');
+const newShoppingForm = document.getElementById('nuevas-compras');
+const incrementShoppingForm = document.getElementById('incremento-compras');
+const newUsersForm = document.getElementById('nuevos-usuarios');
+const newVisitsForm = document.getElementById('nuevas-visitas');
+let data={};
 
 const obtainData = () =>{
   let dashboard = {};
-  dashboard.name = document.getElementById('board-name').value;
-  dashboard.newShopping = document.getElementById('nuevas-compras').value;
-  dashboard.incrementShopping = document.getElementById('incremento-compras').value;
-  dashboard.newUsers = document.getElementById('nuevos-usuarios').value;
-  dashboard.newVisits = document.getElementById('nuevas-visitas').value;
+  dashboard.name = boardNameForm.value;
+  dashboard.newShopping = newUsersForm.value;
+  dashboard.incrementShopping = incrementShoppingForm.value;
+  dashboard.newUsers = newUsersForm.value;
+  dashboard.newVisits = newVisitsForm.value;
   dashboard.dateRegister = new Date();
   return dashboard
 }
@@ -26,6 +30,27 @@ const showData = (data) =>{
   titleVisit.innerHTML = data.newVisits;
 }
 
+const validateBox = () => {
+  if(boardNameForm.value === '' ||
+    newUsersForm.value === '' || newUsersForm.value < 0 ||
+    incrementShoppingForm.value === '' || incrementShoppingForm.value < 0 ||
+    newVisitsForm.value === '' || newVisitsForm.value < 0){
+      return false;
+    }
+    else {
+      return true;
+    }
+}
+
+const clearBox = ()=> {
+  boardNameForm.value = '';
+  newShoppingForm.value = 0;
+  newUsersForm.value = 0;
+  incrementShoppingForm.value = 0;
+  newUsersForm.value = 0;
+  newVisitsForm.value = 0;
+}
+
 // const obtainWeather = () => {
 //   fetch('api.openweathermap.org/data/2.5/weather?q=Mexico City, mx')
 //   .then(response => response.json())
@@ -35,8 +60,15 @@ const showData = (data) =>{
 // };
 
 register.addEventListener('click', () =>{
-  let data = obtainData();
-  showData(data);
+  if(validateBox()){
+    data = obtainData();
+    showData(data);
+    alert('Los datos se han registrado correctamente')
+    clearBox();
+  }
+  else {
+    alert('Para continuar es necesario llenar todos los campos del formulario, recuerde que las cantidades deben ser mayor a 0');
+  }
 
 });
 
